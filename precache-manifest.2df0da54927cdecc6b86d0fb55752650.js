11ece03ba44999e86d43915d0cb8bc7fc299e44b
Leo Gharib <lgharib@zoiclabs.com>
2020-06-27 16:15:05 -0700

    Updates

---
+++ precache-manifest.2df0da54927cdecc6b86d0fb55752650.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691de33513b7d7c0fc129a182db5af69",
+    "url": "/practice-page/index.html"
+  },
+  {
+    "revision": "39d9aa2a40121f9fb3a7",
+    "url": "/practice-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ed06b2b6179662e63254",
+    "url": "/practice-page/static/js/2.864cb000.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/practice-page/static/js/2.864cb000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39d9aa2a40121f9fb3a7",
+    "url": "/practice-page/static/js/main.8cbd6150.chunk.js"
+  },
+  {
+    "revision": "5df544a89e1a0359b23e",
+    "url": "/practice-page/static/js/runtime-main.6c4ba261.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/practice-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
